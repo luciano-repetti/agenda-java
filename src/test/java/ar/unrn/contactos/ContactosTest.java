3a509fdf4aadb58044b73e60913aab1bba02261f
yizuz-maker <jesuscambronero2019@gmail.com>
2024-06-25 19:46:05 -0300

    [EDIT] Update logic for tests

--- src/test/java/ar/unrn/contactos/ContactosTest.java
+++ src/test/java/ar/unrn/contactos/ContactosTest.java
@@ -1,98 +1,94 @@
-// package ar.unrn.contactos;
+package ar.unrn.contactos;
 
-// import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.*;
 
-// @DisplayName("Test de Contactos")
-// class ContactosTest {
+import java.util.List;
 
-// private Contacto contacto1;
-// private Contacto contacto2;
-// private Contacto contacto3;
+@DisplayName("Test de Contactos")
+class ContactosTest {
 
-// @BeforeEach
-// public void setUp() {
-// contacto1 = new Contacto("John", "Doe", "123456789", "john.doe@example.com",
-// "Nota1", "USA", "California", "Los Angeles", "Calle 1");
-// contacto2 = new Contacto(contacto1.id.toString(), "Jane", "Smith",
-// "987654321", "jane.smith@example.com", "Nota2", "USA", "California", "Los
-// Angeles", "Calle 2");
-// contacto3 = new Contacto("Alice", "Johnson", "111111111",
-// "alice.johnson@example.com", "Nota3", "USA", "New York", "New York City",
-// "Calle 3");
-// }
+    private Contacto contacto1;
+    private Contacto contacto2;
+    private Contacto contacto3;
 
-// @DisplayName("Creacion de contacto")
-// @Test
-// public void testCreacionContacto() {
-// Assertions.assertNotNull(contacto1);
-// Assertions.assertEquals("Doe", contacto1.apellido);
-// Assertions.assertEquals("John", contacto1.nombre);
-// Assertions.assertEquals("123456789", contacto1.numeroTelefono);
-// Assertions.assertEquals("john.doe@example.com", contacto1.email);
-// Assertions.assertEquals("Nota1", contacto1.notas);
-// Assertions.assertEquals("USA", contacto1.direccion.pais);
-// Assertions.assertEquals("California", contacto1.direccion.provincia);
-// Assertions.assertEquals("Los Angeles", contacto1.direccion.ciudad);
-// Assertions.assertEquals("Calle 1", contacto1.direccion.calle);
-// }
+    @BeforeEach
+    public void setUp() {
+        contacto1 = new Contacto("John", "Doe", "123456789", "john.doe@example.com",
+                "Nota1", "USA", "California", "Los Angeles", "Calle 1");
+        List<Object> list = contacto1.deshidratarContacto();
+        String uuid = list.get(0).toString();
 
-// @DisplayName("Comparacion de contactos")
-// @Test
-// public void testIgualdadContactos() {
-// Assertions.assertTrue(contacto1.equals(contacto2));
-// Assertions.assertFalse(contacto1.equals(contacto3));
-// }
+        contacto2 = new Contacto(uuid, "Jane", "Smith",
+                "987654321", "jane.smith@example.com", "Nota2", "USA", "California", "Los Angeles", " Calle 2");
 
-// @DisplayName("Modificacion de contacto")
-// @Test
-// public void testModificarContacto() {
-// Contacto nuevoContacto = new Contacto("Bob", "Brown", "222222222",
-// "bob.brown@example.com", "Nota4", "USA", "Nevada", "Las Vegas", "Calle 4");
-// contacto1.modificarContacto(nuevoContacto);
+        contacto3 = new Contacto("Alice", "Johnson", "111111111",
+                "alice.johnson@example.com", "Nota3", "USA", "New York", "New York City",
+                "Calle 3");
+    }
 
-// Assertions.assertEquals("Bob", contacto1.nombre);
-// Assertions.assertEquals("Brown", contacto1.apellido);
-// Assertions.assertEquals("222222222", contacto1.numeroTelefono);
-// Assertions.assertEquals("bob.brown@example.com", contacto1.email);
-// Assertions.assertEquals("Nota4", contacto1.notas);
-// Assertions.assertEquals("USA", contacto1.direccion.pais);
-// Assertions.assertEquals("Nevada", contacto1.direccion.provincia);
-// Assertions.assertEquals("Las Vegas", contacto1.direccion.ciudad);
-// Assertions.assertEquals("Calle 4", contacto1.direccion.calle);
-// }
+    @DisplayName("Creacion de contacto")
+    @Test
+    public void testCreacionContacto() {
+        List<Object> data = contacto1.deshidratarContacto();
+        String nombre = data.get(1).toString();
+        String apellido = data.get(2).toString();
+        String telefono = data.get(3).toString();
+        String email = data.get(4).toString();
+        String notas = data.get(5).toString();
+        String pais = data.get(6).toString();
+        String provincia = data.get(7).toString();
+        String ciudad = data.get(8).toString();
+        String calle = data.get(9).toString();
 
-// @DisplayName("Actualizacion de atributos")
-// @Test
-// public void testActualizarAtributo() {
-// contacto1.actualizarAtributo("nombre", "Michael");
-// contacto1.actualizarAtributo("apellido", "Scott");
-// contacto1.actualizarAtributo("numerotelefono", "333333333");
-// contacto1.actualizarAtributo("email", "michael.scott@example.com");
-// contacto1.actualizarAtributo("notas", "Nota5");
-// contacto1.actualizarAtributo("pais", "Canada");
-// contacto1.actualizarAtributo("provincia", "Ontario");
-// contacto1.actualizarAtributo("ciudad", "Toronto");
-// contacto1.actualizarAtributo("calle", "Calle 5");
 
-// Assertions.assertEquals("Michael", contacto1.nombre);
-// Assertions.assertEquals("Scott", contacto1.apellido);
-// Assertions.assertEquals("333333333", contacto1.numeroTelefono);
-// Assertions.assertEquals("michael.scott@example.com", contacto1.email);
-// Assertions.assertEquals("Nota5", contacto1.notas);
-// Assertions.assertEquals("Canada", contacto1.direccion.pais);
-// Assertions.assertEquals("Ontario", contacto1.direccion.provincia);
-// Assertions.assertEquals("Toronto", contacto1.direccion.ciudad);
-// Assertions.assertEquals("Calle 5", contacto1.direccion.calle);
-// }
+        Assertions.assertNotNull(contacto1);
+        Assertions.assertEquals("John", nombre);
+        Assertions.assertEquals("Doe", apellido);
+        Assertions.assertEquals("123456789", telefono);
+        Assertions.assertEquals("john.doe@example.com", email);
+        Assertions.assertEquals("Nota1", notas);
+        Assertions.assertEquals("USA", pais);
+        Assertions.assertEquals("California", provincia);
+        Assertions.assertEquals("Los Angeles", ciudad);
+        Assertions.assertEquals("Calle 1", calle);
+    }
 
-// @DisplayName("Actualizacion de atributos desconocidos")
-// @Test
-// public void testActualizarAtributoDesconocido() {
-// try {
-// contacto1.actualizarAtributo("atributoDesconocido", "valor");
-// Assertions.fail("No se debería llegar a este punto");
-// } catch (IllegalArgumentException e) {
-// System.out.println("Tedría que haber llegado aca");
-// }
-// }
-// }
+    @DisplayName("Comparacion de contactos")
+    @Test
+    public void testIgualdadContactos() {
+        Assertions.assertTrue(contacto1.equals(contacto2));
+        Assertions.assertFalse(contacto1.equals(contacto3));
+    }
+
+    @DisplayName("Modificacion de contacto")
+    @Test
+    public void testModificarContacto() {
+        Contacto nuevoContacto = new Contacto("Bob", "Brown", "222222222",
+                "bob.brown@example.com", "Nota4", "USA", "Nevada", "Las Vegas", "Calle 4");
+        List<Object> dataTest = nuevoContacto.deshidratarContacto();
+        contacto1.hidratarContacto(dataTest);
+
+
+        List<Object> data = contacto1.deshidratarContacto();
+        String nombre = data.get(1).toString();
+        String apellido = data.get(2).toString();
+        String telefono = data.get(3).toString();
+        String email = data.get(4).toString();
+        String notas = data.get(5).toString();
+        String pais = data.get(6).toString();
+        String provincia = data.get(7).toString();
+        String ciudad = data.get(8).toString();
+        String calle = data.get(9).toString();
+
+        Assertions.assertEquals("Bob", nombre);
+        Assertions.assertEquals("Brown", apellido);
+        Assertions.assertEquals("222222222", telefono);
+        Assertions.assertEquals("bob.brown@example.com", email);
+        Assertions.assertEquals("Nota4", notas);
+        Assertions.assertEquals("USA", pais);
+        Assertions.assertEquals("Nevada", provincia);
+        Assertions.assertEquals("Las Vegas", ciudad);
+        Assertions.assertEquals("Calle 4", calle);
+    }
+
+}
--- src/test/java/ar/unrn/database/DatabaseTest.java
+++ src/test/java/ar/unrn/database/DatabaseTest.java
@@ -1,113 +1,133 @@
-// package ar.unrn.database;
+package ar.unrn.database;
 
-// import ar.unrn.contactos.Contacto;
-// import org.junit.jupiter.api.*;
+import ar.unrn.contactos.Contacto;
+import org.junit.jupiter.api.*;
 
-// import java.sql.SQLException;
-// import java.util.List;
+import java.sql.SQLException;
+import java.util.List;
 
-// @DisplayName("Test de Database")
-// class DatabaseTest {
 
-// private DataBase database;
+@DisplayName("Test de Database")
+class DatabaseTest {
 
-// @DisplayName("Se crea una base de datos en memoria")
-// @BeforeEach
-// public void setUp() throws SQLException {
-// // Usar una base de datos en memoria para pruebas
-// // https://www.sqlite.org/inmemorydb.html
-// database = new DataBase(":memory:");
-// }
+    private DataBase database;
 
-// @AfterEach
-// public void tearDown() throws SQLException {
-// // Cerrar la conexión de la base de datos después de cada prueba
-// database.close();
-// }
+    @DisplayName("Se crea una base de datos en memoria")
+    @BeforeEach
+    public void setUp() throws SQLException {
+        // Usar una base de datos en memoria para pruebas
+        // https://www.sqlite.org/inmemorydb.html
+        database = new DataBase(":memory:");
+    }
 
-// @DisplayName("Envio de contacto")
-// @Test
-// public void testPostContacto() throws SQLException {
-// Contacto contacto = new Contacto("John", "Doe", "1234567890",
-// "john.doe@example.com", "Notas", "Argentina", "Buenos Aires", "CABA", "Calle
-// Falsa 123");
-// database.postContacto(contacto);
+    @AfterEach
+    public void tearDown() throws SQLException {
+        // Cerrar la conexión de la base de datos después de cada prueba
+        database.close();
+    }
 
-// List<Contacto> contactos = database.getContactos(true);
-// Assertions.assertEquals(1, contactos.size());
-// Assertions.assertEquals(contacto, contactos.get(0));
-// }
+    @DisplayName("Envio de contacto")
+    @Test
+    public void testPostContacto() throws SQLException {
+        Contacto contacto = new Contacto("John", "Doe", "1234567890",
+                "john.doe@example.com", "Notas", "Argentina", "Buenos Aires", "CABA",
+                "Calle Falsa 123");
+        database.postContacto(contacto);
 
-// @DisplayName("Eliminacion logica de contacto")
-// @Test
-// public void testDeletearContacto() throws SQLException {
-// Contacto contacto = new Contacto("John", "Doe", "1234567890",
-// "john.doe@example.com", "Notas", "Argentina", "Buenos Aires", "CABA", "Calle
-// Falsa 123");
-// database.postContacto(contacto);
+        List<Contacto> contactos = database.getContactos(true);
+        Assertions.assertEquals(1, contactos.size());
+        Assertions.assertEquals(contacto, contactos.get(0));
+    }
 
-// database.deletear(contacto);
+    @DisplayName("Eliminacion logica de contacto")
+    @Test
+    public void testDeletearContacto() throws SQLException {
+        Contacto contacto = new Contacto("John", "Doe", "1234567890",
+                "john.doe@example.com", "Notas", "Argentina", "Buenos Aires", "CABA",
+                "Calle Falsa 123");
+        database.postContacto(contacto);
 
-// List<Contacto> contactos = database.getContactos(true);
-// Assertions.assertEquals(0, contactos.size());
-// }
+        database.deletear(contacto);
 
-// @DisplayName("Actualizacion contacto")
-// @Test
-// public void testActualizarContacto() throws SQLException {
-// Contacto contacto = new Contacto("John", "Doe", "1234567890",
-// "john.doe@example.com", "Notas", "Argentina", "Buenos Aires", "CABA", "Calle
-// Falsa 123");
-// database.postContacto(contacto);
+        List<Contacto> contactos = database.getContactos(true);
+        Assertions.assertEquals(0, contactos.size());
+    }
 
-// Contacto contactoActualizado = new Contacto(contacto.id.toString(), "John",
-// "Doe", "0987654321", "john.doe@newexample.com", "Nuevas Notas", "Argentina",
-// "Buenos Aires", "CABA", "Calle Verdadera 456");
-// database.actualizar(contactoActualizado);
+    @DisplayName("Actualizacion contacto")
+    @Test
+    public void testActualizarContacto() throws SQLException {
+        Contacto contacto = new Contacto("John", "Doe", "1234567890",
+                "john.doe@example.com", "Notas", "Argentina", "Buenos Aires", "CABA",
+                "Calle Falsa 123");
+        database.postContacto(contacto);
 
-// List<Contacto> contactos = database.getContactos(true);
-// Assertions.assertEquals(1, contactos.size());
-// Assertions.assertEquals(contactoActualizado.numeroTelefono,
-// contactos.get(0).numeroTelefono);
-// Assertions.assertEquals(contactoActualizado.email, contactos.get(0).email);
-// Assertions.assertEquals(contactoActualizado.notas, contactos.get(0).notas);
-// Assertions.assertEquals(contactoActualizado.direccion.calle,
-// contactos.get(0).direccion.calle);
-// }
+        List<Object> data = contacto.deshidratarContacto();
+        String uuid = data.get(0).toString();
+        System.out.println("UUID: " + uuid);
 
-// @DisplayName("Eliminacion logica de contacto")
-// @Test
-// public void testLogicalDeleteContacto() throws SQLException {
-// Contacto contacto = new Contacto("John", "Doe", "1234567890",
-// "john.doe@example.com", "Notas", "Argentina", "Buenos Aires", "CABA", "Calle
-// Falsa 123");
-// database.postContacto(contacto);
+        Contacto contactoActualizado = new Contacto(uuid, "John",
+                "Doe", "0987654321", "john.doe@newexample.com", "Nuevas Notas", "Argentina",
+                "Buenos Aires", "CABA", "Calle Verdadera 456");
 
-// database.logicalDeleteContacto(contacto.id.toString());
+        database.actualizar(contactoActualizado);
 
-// List<Contacto> contactos = database.getContactos(true);
-// Assertions.assertEquals(0, contactos.size());
+        List<Contacto> contactos = database.getContactos(true);
 
-// List<Contacto> contactosInactivos = database.getContactos(false);
-// Assertions.assertEquals(1, contactosInactivos.size());
-// Assertions.assertEquals(contacto, contactosInactivos.get(0));
-// }
+        List<Object> data1 = contactos.get(0).deshidratarContacto();
+        String email = data1.get(4).toString();
+        Assertions.assertEquals(1, contactos.size());
+        Assertions.assertEquals(email, "john.doe@newexample.com");
 
-// @DisplayName("Obtener contactos")
-// @Test
-// public void testGetContactos() throws SQLException {
-// Contacto contacto1 = new Contacto("John", "Doe", "1234567890",
-// "john.doe@example.com", "Notas", "Argentina", "Buenos Aires", "CABA", "Calle
-// Falsa 123");
-// Contacto contacto2 = new Contacto("Jane", "Smith", "0987654321",
-// "jane.smith@example.com", "Notas2", "Argentina", "Buenos Aires", "CABA",
-// "Calle Falsa 456");
-// database.postContacto(contacto1);
-// database.postContacto(contacto2);
+        String calle = data1.get(9).toString();
+        Assertions.assertEquals(calle, "Calle Verdadera 456");
 
-// List<Contacto> contactos = database.getContactos(true);
-// Assertions.assertEquals(2, contactos.size());
-// Assertions.assertTrue(contactos.contains(contacto1));
-// Assertions.assertTrue(contactos.contains(contacto2));
-// }
-// }
+        String telefono = data1.get(3).toString();
+        Assertions.assertEquals(telefono, "0987654321");
+
+        String notas = data1.get(5).toString();
+        Assertions.assertEquals(notas, "Nuevas Notas");
+
+        String street = data1.get(9).toString();
+        Assertions.assertEquals(street, "Calle Verdadera 456");
+
+    }
+
+    @DisplayName("Eliminacion logica de contacto")
+    @Test
+    public void testLogicalDeleteContacto() throws SQLException {
+        Contacto contacto = new Contacto("John", "Doe", "1234567890",
+                "john.doe@example.com", "Notas", "Argentina", "Buenos Aires", "CABA", "Calle " +
+                "Falsa 123");
+        database.postContacto(contacto);
+        List<Object> data = contacto.deshidratarContacto();
+        String uuid = data.get(0).toString();
+
+
+        database.logicalDeleteContacto(uuid);
+
+        List<Contacto> contactos = database.getContactos(true);
+        Assertions.assertEquals(0, contactos.size());
+
+        List<Contacto> contactosInactivos = database.getContactos(false);
+        Assertions.assertEquals(1, contactosInactivos.size());
+        Assertions.assertEquals(contacto, contactosInactivos.get(0));
+    }
+
+    @DisplayName("Obtener contactos")
+    @Test
+    public void testGetContactos() throws SQLException {
+        Contacto contacto1 = new Contacto("John", "Doe", "1234567890",
+                "john.doe@example.com", "Notas", "Argentina", "Buenos Aires", "CABA", "Calle Falsa 123");
+
+        Contacto contacto2 = new Contacto("Jane", "Smith", "0987654321",
+                "jane.smith@example.com", "Notas2", "Argentina", "Buenos Aires", "CABA",
+                "Calle Falsa 456");
+        database.postContacto(contacto1);
+        database.postContacto(contacto2);
+
+        List<Contacto> contactos = database.getContactos(true);
+        Assertions.assertEquals(2, contactos.size());
+        Assertions.assertTrue(contactos.contains(contacto1));
+        Assertions.assertTrue(contactos.contains(contacto2));
+    }
+}
